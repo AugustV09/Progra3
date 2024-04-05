48bed81085a7938325ab19434f3d1ddc2bab1136
Octavio <valenzi.octavio@gmail.com>
2024-04-05 17:54:27 -0300

    Optimizando los ejercicios de la practica 2

--- AYED/tp2/ejercicio8/ParcialArboles.java
+++ AYED/tp2/ejercicio8/ParcialArboles.java
@@ -31,4 +31,32 @@ public class ParcialArboles {
 			 return resultado;
 		 }
 	 }
+	 
+	 public boolean esPrefijo2(BinaryTree<Integer> arbol1, BinaryTree<Integer> arbol2) {
+		 if (arbol1 == null && arbol2 == null) return true;			//Primero aplico los posibles casos de corte de control y luego evaluo el caso mas comun
+		 
+		 else if (arbol1 == null || arbol2 == null) return false;
+		 
+		 else if (arbol1.isEmpty() && arbol2.isEmpty()) return true;
+		 
+		 else if (arbol1.isEmpty() || arbol2.isEmpty()) return false;
+		 
+		 else {
+			 boolean resultado = true;
+			 
+			 if (arbol1.getData()!= arbol2.getData()) resultado = false;
+			 
+			 if (resultado) {
+				 if (arbol1.hasLeftChild()) resultado = esPrefijo2(arbol1.getLeftChild(),arbol2.getRightChild());
+				 
+				 if (resultado) {
+					 
+					 if(arbol1.hasRightChild()) resultado = esPrefijo2(arbol1.getRightChild(), arbol2.getRightChild());
+				 }
+			 }
+			 
+			 
+			 return resultado;
+		 }
+	 }
 }
