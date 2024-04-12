6098edb712377e7311c675780a28d8916a781d05
Octavio <valenzi.octavio@gmail.com>
2024-04-11 22:16:39 -0300

    Arrancando con la practica 3

---
+++ AYED/tp3/ejercicio1/GeneralTree.java
@@ -0,0 +1,73 @@
+package tp3.ejercicio1;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GeneralTree<T> {
+
+	private T data;
+	private List<GeneralTree<T>> children = new LinkedList<GeneralTree<T>>();
+	
+	public GeneralTree(T data){
+		this.data = data;
+	}
+	
+	public GeneralTree(T data, List<GeneralTree<T>> children){
+		this.data = data;
+		this.children = children;
+	}
+	
+	public GeneralTree(){
+		
+	}
+	
+	public T getData(){
+		return this.data;
+	}
+	
+	public void setData (T data){
+		this.data = data;
+	}
+	
+	public List<GeneralTree<T>> getChildren(){
+		return this.children;
+	}
+	
+	public void setChildren (List<GeneralTree<T>> children){
+		this.children = children;
+	}
+	
+	public void addChild (GeneralTree<T> child){
+		getChildren().add(child);
+	}
+	
+	public boolean hasChildren(){
+		return (getChildren()!= null && !getChildren().isEmpty());
+	}
+	
+	public boolean isLeaf(){
+		return (!hasChildren());
+	}
+	
+	public boolean isEmpty(){
+		return (getData()==null && isLeaf());
+	}
+	
+	public void removeChild(GeneralTree<T> child){
+		if (this.hasChildren()){
+			getChildren().remove(child);
+		}
+	}
+	
+
+	public boolean equals (GeneralTree<T> ga){
+		boolean resultado = false;
+		
+		if (ga!= null){
+			if (this.getData().equals(ga.getData()) && this.getChildren().equals(ga.getChildren())) resultado = true;
+		}
+		
+		return resultado;
+	}
+	
+}
