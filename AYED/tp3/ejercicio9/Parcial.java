c51513a0c6b0cb4449f25f75affe2bb2502ecfe2
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2024-04-22 19:30:35 -0300

    Haciendo hasta el ejercicio 9

---
+++ AYED/tp3/ejercicio9/Parcial.java
@@ -0,0 +1,74 @@
+package tp3.ejercicio9;
+
+import tp1.ejercicio8.Queue;
+import tp3.ejercicio1.GeneralTree;
+
+public class Parcial {
+
+	public static boolean esDeSeleccion (GeneralTree<Integer> arbol) {
+		
+		return esDeSeleccion_private(arbol,arbol.getData());
+		
+	}
+	
+	private static boolean esDeSeleccion_private (GeneralTree<Integer> arbol,int raiz) {
+		
+		if (arbol.hasChildren()) {
+			boolean resultado= false;
+			int min = Integer.MAX_VALUE;
+			
+			if (raiz <= arbol.getData()) {
+				
+				resultado= true;
+				
+				for (GeneralTree<Integer> i : arbol.getChildren()) {
+					resultado = resultado && esDeSeleccion_private(i,arbol.getData());
+					if (!resultado) break;
+					if (min > i.getData()) min = i.getData();				}
+				
+			} 
+			
+			return (resultado && (min == raiz));
+		}
+		
+		return true;
+		
+	}
+	
+	public static boolean esDeSeleccion2 (GeneralTree<Integer> arbol) {
+		
+		//Si el arbol es NULL o VACIO, QUE RETORNO??
+		
+		Queue<GeneralTree<Integer>> cola = new Queue<GeneralTree<Integer>>();
+		GeneralTree<Integer> tree_aux;
+		
+		cola.enqueue(arbol);
+		
+		int min = Integer.MAX_VALUE;
+		
+		while (!cola.isEmpty()) {
+			
+			tree_aux = cola.dequeue();
+			
+			if (tree_aux.hasChildren()) {
+				for (GeneralTree<Integer> i : tree_aux.getChildren()) {
+					if (i.getData() < arbol.getData()) return false;
+					if (i.getData()< min) min = i.getData();
+					cola.enqueue(i);
+				}
+				
+				if (min != tree_aux.getData()) return false;
+			}
+			
+		}
+		
+		return true;
+		
+		
+	}
+	
+	
+	
+	
+	
+}
